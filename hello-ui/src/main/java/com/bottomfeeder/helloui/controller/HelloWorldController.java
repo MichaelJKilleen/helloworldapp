4ec892d0dbb09d4aca5c4ea39ce003c8bf43e72e
Michael Killeen <michaeljkilleen@outlook.com>
2021-03-28 12:43:56 -0400

    adding hello-ui

---
+++ hello-ui/src/main/java/com/bottomfeeder/helloui/controller/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.bottomfeeder.helloui.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+@RequestMapping("hello-world")
+public class HelloWorldController {
+
+    public static final String FOO_RESOURCE_URL = "http://localhost:8082/rest/service/greeting";
+
+    @RequestMapping(value = "/index")
+    public String index(Model model) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<String> response
+                = restTemplate.getForEntity(FOO_RESOURCE_URL, String.class);
+
+        model.addAttribute("greeting",response.getBody());
+
+        return "index";
+    }
+}
